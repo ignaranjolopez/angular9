e27d5826b34bbd8cddf386070d6fdd18a643952e
ignacio.naranjo <ignacio.naranjo@apiservice.cl>
2020-06-06 18:24:09 -0400

    trabajando con ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
@@ -10,7 +11,8 @@ import { EjerciciosComponent } from './ejercicios/ejercicios.component';
     EjerciciosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
